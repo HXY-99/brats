65503bd7b7f15913911a877b11620f07af501bc9
hxy99 <78461236+HXY-99@users.noreply.github.com>
2024-06-04 19:15:47 +0800

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -7,32 +7,19 @@ import torch.utils.data
 from torchvision import datasets, models, transforms
 
 
-class Dataset(torch.utils.data.Dataset):
+class BraTS(data.Dataset):
 
-    def __init__(self, img_paths, edge_paths, mask_paths, edge_mask_paths):
-        self.seg_img_paths = img_paths
-        self.edge_img_paths = edge_paths
-        self.seg_mask_paths = mask_paths
-        self.edge_mask_paths = edge_mask_paths
+    def __init__(self, root):
+        self.root = root
+        self.file_list = os.listdir(root)
 
     def __len__(self):
-        return len(self.seg_img_paths)
+        return len(self.file_list)
 
     def __getitem__(self, idx):
-        seg_img_path = self.seg_img_paths[idx]
-        edge_img_path = self.edge_img_paths[idx]
-        seg_mask_path = self.seg_mask_paths[idx]
-        edge_mask_path = self.edge_mask_paths[idx]
 
-        image = np.load(seg_img_path)
-        edge = np.load(edge_img_path)
-        mask = np.load(seg_mask_path)
-        edge_mask = np.load(edge_mask_path)
-
-        seg_image = image.transpose((2, 0, 1))
-        seg_image = seg_image.astype("float32")
-        edge_image = edge.transpose((2, 0, 1))
-        edge_image = edge_image.astype("float32")
+        file = self.file_list[idx]
+        img, mask = pkload(os.path.join(self.root, file))
 
         WT_Label = mask.copy()
         WT_Label[mask == 1] = 1.
@@ -46,19 +33,13 @@ class Dataset(torch.utils.data.Dataset):
         ET_Label[mask == 1] = 0.
         ET_Label[mask == 2] = 0.
         ET_Label[mask == 4] = 1.
-        seg_label = np.empty((240, 240, 3))
-        seg_label[:, :, 0] = WT_Label
-        seg_label[:, :, 1] = TC_Label
-        seg_label[:, :, 2] = ET_Label
-        seg_label = seg_label.transpose((2, 0, 1))
+        label = np.empty((3, 160, 160))
+        label[0, ...] = WT_Label
+        label[1, ...] = TC_Label
+        label[2, ...] = ET_Label
 
-        edge_label = np.empty((240, 240, 1))
-        edge_label[:, :, 0] = edge_mask
-        edge_label = edge_label.transpose((2, 0, 1))
-
-        seg_label = seg_label.astype("float32")
-        edge_label = edge_label.astype("float32")
-
-        return seg_image, edge_image, seg_label, edge_label
+        img = np.ascontiguousarray(img.transpose(2, 0, 1))
+        
+        return img, label
 
 
